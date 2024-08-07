ddf4b8cee85fda70bd7abced50840f1ef015b6e2
hahmadqayyum <hahmadqayyum@gmail.com>
2024-08-07 06:42:57 -0700

    installed and setup typescript in project - expressJS

--- routes/user.ts
+++ routes/user.ts
@@ -2,7 +2,15 @@ var express = require("express");
 var router = express.Router();
 // const { v4: uuidv4 } = require('uuid');
 
-var usersDB = [];
+type User = {
+  name: string,
+  email: string,
+  phoneNo: string,
+  password: string
+}
+var usersDB : Array<User> = [];
+
+
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -18,16 +26,22 @@ router.get("/", function (req, res, next) {
 router.put("/userRegister", (req, res) => {
   const { name, email, phoneNo, password } = req.body;
 
+  
+
   if (!name || !email || !phoneNo || !password) {
     res.status(400);
     res.json({ message: "BAD request" });
   } else {
-    usersDB.push({
+    const user: User = {
       email: email,
-      name: name,
       phoneNo: phoneNo,
-      password: password,
-    });
+      name: name,
+      password: password
+    }
+    
+
+
+    usersDB.push(user);
     res.json({ message: `User created with email ${email}` });
   }
 });
